e99e044799e490f9b45dfacf4a165dd3f6320256
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-08-01 12:18:18 -0700

    ran my first c++ code in a while

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+
+int main(int argc, const char *argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::string name = "Nnaemeka";
+    std::cout << "My name is " << name << "\n";
+
+    return 0;
+}
