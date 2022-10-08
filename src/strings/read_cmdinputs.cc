15c4cf63560faf162540ffd7c802183ba9ebdc98
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-10-08 00:22:03 -0700

    dabbling with strings

---
+++ src/strings/read_cmdinputs.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string content;
+    std::string line;
+    std::cout << "Please introduce a text. Enter an empty line to finish: \n";
+
+    do {
+        getline(std::cin, line);
+        content += line + '\n';
+    } while (!line.empty());
+
+    std::cout << "The text you introduced was: \n" << content;
+    return 0;
+}
+
+// TODO: Learn how to get command line input separated by spaces for unspecified row and column length
